d7490a25d8cbe0be18c311009695fcab159a27e3
oscarscode <hamish.oscar@gmail.com>
2018-06-20 15:55:01 +0530

    Moved all projects into learnpython

---
+++ HelloWorld.py
@@ -0,0 +1,33 @@
+# print('Hello, World!')
+# print(1 + 2)
+# print(7 + 6)
+# print()
+# print("The End")
+# print("Hello" + " World")
+
+# greeting = "Hello"
+# name = input("Please enter your name")
+# print(greeting + name)
+
+# splitstring = "This string has been\nsplit over\nserveral\nlines"
+# print(splitstring)
+#
+# tabbedString= "\t1\t2\t3\t4"
+# print(tabbedString)
+
+print('The pet shop owner said "No, no,\'e\'s uh....he\'s resting"')
+print("The pet shop owner said \"No, no,'e's uh....he's resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+
+print(anotherSplitString)
+
+print("""The pet shop owner said "No, no,'e's uh....he's resting" """)
+
+print(""" Adding some
+new lines
+for
+testing the
+split!""")
---
+++ learn1.py
@@ -0,0 +1,4 @@
+print("My hovercraft is full of eels")
+print(6 * 7)
+
+print("lets see....")
