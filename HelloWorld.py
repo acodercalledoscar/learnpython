cc28e9efdf65606e072600d5ebea3f3d374cd79b
oscarscode <hamish.oscar@gmail.com>
2018-06-20 16:04:43 +0530

    revision

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,8 +6,8 @@
 # print("Hello" + " World")
 
 # greeting = "Hello"
-# name = input("Please enter your name")
-# print(greeting + name)
+# name = input("Please enter your name: ")
+# print(greeting + " " + name)
 
 # splitstring = "This string has been\nsplit over\nserveral\nlines"
 # print(splitstring)
@@ -15,14 +15,14 @@
 # tabbedString= "\t1\t2\t3\t4"
 # print(tabbedString)
 
-print('The pet shop owner said "No, no,\'e\'s uh....he\'s resting"')
-print("The pet shop owner said \"No, no,'e's uh....he's resting\"")
+# print('The pet shop owner said "No, no,\'e\'s uh....he\'s resting"')
+# print("The pet shop owner said \"No, no,'e's uh....he's resting\"")
 
-anotherSplitString = """This string has been
-split over
-several lines"""
-
-print(anotherSplitString)
+# anotherSplitString = """This string has been
+# split over
+# several lines"""
+#
+# print(anotherSplitString)
 
 print("""The pet shop owner said "No, no,'e's uh....he's resting" """)
 
--- Variables.py
+++ Variables.py
@@ -4,24 +4,25 @@
 # ham_was_57 = "Hello"
 # Greeting = "Lawrence"
 # print(ham_was_57 + ' ' + greeting + " " + Greeting)
+# #print(57 + ' ' + greeting + " " + Greeting)
 #
 # _age = 37
 # print(_age)
 #
 # # print(greeting + age)
 
-# a = 12
-# b = 3
+a = 12
+b = 3
 # print(a + b)
 # print(a - b)
 # print(a * b)
 # print(a / b)
 # print(a // b)
 # print(a % b)
-# #
-# for i in range(1, a // b):
-#     print(i)
 #
+for i in range(0, a // b):
+    print(i)
+
 # print(a + b / 3 - 4 * 12)
 # print(((((a + b) // 3) - 4) * 12))
 # c = (a + b)
@@ -31,32 +32,32 @@
 
 
 
-parrot = "Norwegian Blue"
-print(parrot)
-print(parrot[0])
-print(parrot[3])
-print(parrot[-4])
-print(parrot[0:6])
-print(parrot[:6])
-print(parrot[6:])
-print(parrot[-4:-2])
-print(parrot[0:6:2])
-print(parrot[0:6:3])
-
-number = "9,234,654,134,745"
-print(number[1::4])
-numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
-print(numbers[0::3])
-
-string1 = "he's "
-string2 = "probably "
-print(string1 + string2)
-
-print("Hello "* 5)
-print("Hello "*(5 + 4))
-print("Hello "* 5 + "4")
-today = "Friday"
-print("day" in today)
-print("Fri" in today)
-print("Thu" in today)
-print("parrot" in "Fjord")
+# parrot = "Norwegian Blue"
+# print(parrot)
+# print(parrot[0])
+# print(parrot[3])
+# print(parrot[-4])
+# print(parrot[0:6])
+# print(parrot[:6])
+# print(parrot[6:])
+# print(parrot[-4:-2])
+# print(parrot[0:6:2])
+# print(parrot[0:6:3])
+#
+# number = "9,234,654,134,745"
+# print(number[1::4])
+# numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+# print(numbers[0::3])
+#
+# string1 = "he's "
+# string2 = "probably "
+# print(string1 + string2)
+#
+# print("Hello "* 5)
+# print("Hello "*(5 + 4))
+# print("Hello "* 5 + "4")
+# today = "Friday"
+# print("day" in today)
+# print("Fri" in today)
+# print("Thu" in today)
+# print("parrot" in "Fjord")
