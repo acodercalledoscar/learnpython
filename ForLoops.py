2d7c1df62b0977fd6b4e62abb81785e8eb34dc47
oscarscode <hamish.oscar@gmail.com>
2018-06-21 17:35:34 +0530

    ForLoops

---
+++ ForLoops.py
@@ -0,0 +1,19 @@
+# for i in range(1,20):
+#     print("i is now {}".format(i))
+
+# number = "92,233,720,368,854,775,807"
+# for i in range(0,len(number)):
+#     print(number[i])
+
+# number = "92,233,720,368,854,775,807"
+# cleanedNumber = " "
+#
+# for i in range(0,len(number)):
+#     if number[i] in "0123456789":
+#         cleanedNumber = cleanedNumber + number[i]
+#         # print(number[i],end="")
+# newNumber = int(cleanedNumber)
+# print("The number is {}".format(newNumber))
+
+for i in range(10):
+    print(i)
