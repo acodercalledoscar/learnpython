b2274259adb11230601e700df6fa933e7e0d77d2
oscarscode <hamish.oscar@gmail.com>
2018-07-08 11:53:20 +0530

    ForLoops lecture 30

--- ForLoops.py
+++ ForLoops.py
@@ -15,5 +15,28 @@
 # newNumber = int(cleanedNumber)
 # print("The number is {}".format(newNumber))
 
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
+
+
+# number = "92,233,720,368,854,775,807"
+# cleanedNumber = " "
+#
+# for char in number:
+#     if char in "0123456789":
+#         cleanedNumber = cleanedNumber + char
+# newNumber = int(cleanedNumber)
+# print("The number is {}".format(newNumber))
+
+# for state in ("not pinin'","no more","a stiff","bereft of life"):
+#     print("This parrot is " + state)
+
+
+# for i in range(0, 100, 5):
+#     print("i is {}".format(i))
+
+for i in range(1,13):
+    for j in range(1,13):
+        print("{0} times {1} is {2}".format(i, j, i*j), end="\t")
+    #print("=========================")
+    print("")
